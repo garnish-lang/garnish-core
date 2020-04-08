b9c3d0ff4a2e46bbce429634e2801bd84a0cc80c
Chad Collins <chadrcollins91@gmail.com>
2020-04-08 18:51:17 -0400

    Correctly ordering dot precedence when used together

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -25,37 +25,68 @@ pub fn make_ast(mut parse_result: ParseResult) -> Result<AST> {
         });
     }
 
-    let mut op_locations: Vec<usize> = vec![];
+    let mut op_locations: Vec<Vec<usize>> = vec![
+        vec![],
+        vec![]
+    ];
 
     for (i, node) in parse_result.nodes.iter().enumerate() {
-        if node.classification == Classification::Decimal {
-            op_locations.push(i);
-        }
+        let p = match node.classification {
+            Classification::Literal 
+            | Classification::IterationOutput 
+            | Classification::IterationSkip 
+            | Classification::IterationContinue
+            | Classification::IterationComplete => continue,
+            Classification::Decimal => 0,
+            Classification::Access => 1,
+            _ => unimplemented!()
+        };
+
+        op_locations[p].push(i);
     }
 
-    for loc in op_locations.iter() {
-        // get op's left and right
-        // update parent to be loc
-        // if value set left and right to None
+    for precedence in op_locations.iter() {
+        for loc in precedence.iter() {
+            // get op's left and right
+            // update parent to be loc
+            // if value set left and right to None
 
-        let (left, right) = parse_result.nodes.get(*loc).map(|n| (n.left, n.right)).unwrap();
+            let (left, right) = parse_result.nodes.get(*loc).map(|n| (n.left, n.right)).unwrap();
 
-        match left {
-            Some(i) => {
-                parse_result.nodes[i].parent = Some(*loc);
-                parse_result.nodes[i].left = None;
-                parse_result.nodes[i].right = None;
+            match left {
+                Some(i) => {
+                    parse_result.nodes[i].parent = Some(*loc);
+
+                    if parse_result.nodes[i].classification == Classification::Literal {
+                        parse_result.nodes[i].left = None;
+                        parse_result.nodes[i].right = None;
+                    }
+                }
+                None => () // nothing to do
             }
-            None => () // nothing to do
-        }
 
-        match right {
-            Some(i) => {
-                parse_result.nodes[i].parent = Some(*loc);
-                parse_result.nodes[i].left = None;
-                parse_result.nodes[i].right = None;
+            let new_right = match right {
+                Some(i) => {
+                    let r = parse_result.nodes[i].right;
+                    parse_result.nodes[i].parent = Some(*loc);
+
+                    if parse_result.nodes[i].classification == Classification::Literal {
+                        parse_result.nodes[i].left = None;
+                        parse_result.nodes[i].right = None;
+                    }
+
+                    r
+                }
+                None => None // nothing to do
+            };
+            
+            // update this right node's left to point to this node
+            match new_right {
+                Some(r) => {
+                    parse_result.nodes[r].left = Some(*loc);
+                }
+                None => () // nothing to update
             }
-            None => () // nothing to do
         }
     }
 
@@ -160,6 +191,42 @@ mod value_precedence_tests {
             token: Token {
                 value: String::from("hello world"),
                 token_type: TokenType::CharacterList,
+            },
+            left: None,
+            right: None,
+            parent: None
+        }]);
+        assert_eq!(ast.root, 0);
+        assert_eq!(ast.sub_roots, vec![]);
+    }
+
+    #[test]
+    fn identifier_only() {
+        let ast = ast_from("my_value");
+
+        assert_eq!(ast.nodes, vec![Node {
+            classification: Classification::Literal,
+            token: Token {
+                value: String::from("my_value"),
+                token_type: TokenType::Identifier,
+            },
+            left: None,
+            right: None,
+            parent: None
+        }]);
+        assert_eq!(ast.root, 0);
+        assert_eq!(ast.sub_roots, vec![]);
+    }
+
+    #[test]
+    fn symbol_only() {
+        let ast = ast_from(":");
+
+        assert_eq!(ast.nodes, vec![Node {
+            classification: Classification::Literal,
+            token: Token {
+                value: String::from(":"),
+                token_type: TokenType::SymbolOperator,
             },
             left: None,
             right: None,
@@ -321,5 +388,59 @@ mod dot_access_precedence_tests {
         assert_eq!(node.right, Some(2));
 
         assert_eq!(ast.root, 1);
+    }
+
+    #[test]
+    fn access_is_above_identifiers() {
+        let ast = ast_from("my_object.my_value");
+
+        let node = ast.nodes.get(0).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(2).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(1).unwrap();
+        assert_eq!(node.parent, None);
+        assert_eq!(node.left, Some(0));
+        assert_eq!(node.right, Some(2));
+
+        assert_eq!(ast.root, 1);
+    }
+
+    #[test]
+    fn access_is_above_decimal() {
+        let ast = ast_from("3.14.my_value");
+
+        let node = ast.nodes.get(0).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(1).unwrap();
+        assert_eq!(node.parent, Some(3));
+        assert_eq!(node.left, Some(0));
+        assert_eq!(node.right, Some(2));
+
+        let node = ast.nodes.get(2).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(3).unwrap();
+        assert_eq!(node.parent, None);
+        assert_eq!(node.left, Some(1));
+        assert_eq!(node.right, Some(4));
+
+        let node = ast.nodes.get(4).unwrap();
+        assert_eq!(node.parent, Some(3));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        assert_eq!(ast.root, 3);
     }
 }
