89f9d9985addaf35830b986e36fe44d0ea8f9b5a
Chad Collins <chadrcollins91@gmail.com>
2020-04-08 15:47:30 -0400

    Creating AST with decimal node.

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -7,7 +7,7 @@ pub struct AST {
     pub(crate) sub_roots: Vec<usize>
 }
 
-pub fn make_ast(parse_result: ParseResult) -> Result<AST> {
+pub fn make_ast(mut parse_result: ParseResult) -> Result<AST> {
     if parse_result.nodes.is_empty() {
         return Ok(AST {
             nodes: vec![Node {
@@ -25,15 +25,51 @@ pub fn make_ast(parse_result: ParseResult) -> Result<AST> {
         });
     }
 
-    let mut nodes = Vec::with_capacity(parse_result.nodes.len());
+    let mut op_locations: Vec<usize> = vec![];
 
-    for node in parse_result.nodes.iter() {
-        nodes.push(node.clone());
+    for (i, node) in parse_result.nodes.iter().enumerate() {
+        if node.classification == Classification::Decimal {
+            op_locations.push(i);
+        }
+    }
+
+    for loc in op_locations.iter() {
+        // get op's left and right
+        // update parent to be loc
+        // if value set left and right to None
+
+        let (left, right) = parse_result.nodes.get(*loc).map(|n| (n.left, n.right)).unwrap();
+
+        match left {
+            Some(i) => {
+                parse_result.nodes[i].parent = Some(*loc);
+                parse_result.nodes[i].left = None;
+                parse_result.nodes[i].right = None;
+            }
+            None => () // nothing to do
+        }
+
+        match right {
+            Some(i) => {
+                parse_result.nodes[i].parent = Some(*loc);
+                parse_result.nodes[i].left = None;
+                parse_result.nodes[i].right = None;
+            }
+            None => () // nothing to do
+        }
+    }
+
+    let mut root_index = *parse_result.sub_expressions.get(0).unwrap(); // should always have 1
+    let mut node = &parse_result.nodes[root_index];
+
+    while node.parent.is_some() {
+        root_index = node.parent.unwrap();
+        node = &parse_result.nodes[root_index];
     }
 
     return Ok(AST {
-        nodes,
-        root: 0,
+        nodes: parse_result.nodes.clone(),
+        root: root_index,
         sub_roots: vec![]
     });
 }
@@ -76,8 +112,9 @@ mod tests {
 
 #[cfg(test)]
 mod value_precedence_tests {
-    use crate::{make_ast, AST, Lexer, TokenType, Token, Node, Parser, Classification};
+    use crate::{Lexer, TokenType, Token, Node, Parser, Classification};
     use super::tests::ast_from;
+    
     #[test]
     fn number_only() {
         let ast = ast_from("10");
@@ -261,5 +298,28 @@ mod value_precedence_tests {
 
 #[cfg(test)]
 mod dot_access_precedence_tests {
+    use crate::{Lexer, TokenType, Token, Node, Parser, Classification};
+    use super::tests::ast_from;
 
+    #[test]
+    fn decimal_is_above_numbers() {
+        let ast = ast_from("3.14");
+
+        let node = ast.nodes.get(0).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(2).unwrap();
+        assert_eq!(node.parent, Some(1));
+        assert_eq!(node.left, None);
+        assert_eq!(node.right, None);
+
+        let node = ast.nodes.get(1).unwrap();
+        assert_eq!(node.parent, None);
+        assert_eq!(node.left, Some(0));
+        assert_eq!(node.right, Some(2));
+
+        assert_eq!(ast.root, 1);
+    }
 }
