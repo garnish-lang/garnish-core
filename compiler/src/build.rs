7fbd20e8914ed67905f2ea130904c230f1c2d858
Chad Collins <chadrcollins91@gmail.com>
2020-04-17 07:24:20 -0400

    Byte code for access and multi access ASTs

--- compiler/src/build.rs
+++ compiler/src/build.rs
@@ -13,51 +13,64 @@ pub fn build_byte_code(ast: AST) -> Result<InstructionSetBuilder> {
         return Ok(instructions);
     }
 
-    let extract = |o, s, p| -> Result<&Node> { 
+    process_node(ast.root, &ast, &mut instructions)?;
+
+    instructions.end_expression();
+
+    return Ok(instructions);
+}
+
+fn process_node(index: usize, ast: &AST, instructions: &mut InstructionSetBuilder) -> Result<()> {
+    let extract_index = |o, s, p| -> Result<usize> {
         match o {
-            Some(i) => match ast.nodes.get(i) {
-                Some(n) => Ok(n),
-                None => Err(format!("Node on {} side of parent {} with index {} not in AST", s, p, i).into())
-            }
+            Some(i) => Ok(i),
             None => Err(format!("Expected {} side node for parent {}", s, p).into())
+        }
+    };
+
+    let extract = |o, s, p| -> Result<&Node> { 
+        let i = extract_index(o, s, p)?;
+        match ast.nodes.get(i) {
+            Some(n) => Ok(n),
+            None => Err(format!("Node on {} side of parent {} with index {} not in AST", s, p, i).into())
         }
     };
 
     let extract_left = |o, p| extract(o, "left", p);
     let extract_right = |o, p| extract(o, "right", p);
-
-    let first = &ast.nodes[ast.root];
-    match first.classification {
+    
+    let node = &ast.nodes[index];
+    match node.classification {
         // put literal values in based on their type
-        Classification::Literal => match first.token.token_type {
+        Classification::Literal => match node.token.token_type {
             TokenType::UnitLiteral => instructions.put(ExpressionValue::unit())?,
             TokenType::Number => {
-                let i: i32 = match first.token.value.parse() {
+                let i: i32 = match node.token.value.parse() {
                     Ok(f) => f,
-                    Err(_) => return Err(format!("Invalid integer value ({}) at node {}", first.token.value, ast.root).into())
+                    Err(_) => return Err(format!("Invalid integer value ({}) at node {}", node.token.value, ast.root).into())
                 };
                 instructions.put(ExpressionValue::integer(i))?;
             }
             TokenType::Character => {
-                instructions.put(ExpressionValue::character(first.token.value.clone()))?;
+                instructions.put(ExpressionValue::character(node.token.value.clone()))?;
             }
             TokenType::CharacterList => {
-                instructions.put(ExpressionValue::character_list(first.token.value.clone()))?;
+                instructions.put(ExpressionValue::character_list(node.token.value.clone()))?;
             }
             TokenType::Identifier => {
-                instructions.resolve(&first.token.value);
+                instructions.resolve(&node.token.value);
             }
             _ => unimplemented!()
         }
         Classification::Symbol => {
             // unary op literal
-            instructions.put(ExpressionValue::symbol(&extract_right(first.right, ast.root)?.token.value))?;
+            instructions.put(ExpressionValue::symbol(&extract_right(node.right, ast.root)?.token.value))?;
         }
         Classification::Decimal => {
             // special literal value composed of two literal nodes
             let float_str = format!("{}.{}", 
-                extract_left(first.left, ast.root)?.token.value, 
-                extract_right(first.right, ast.root)?.token.value
+                extract_left(node.left, ast.root)?.token.value, 
+                extract_right(node.right, ast.root)?.token.value
             );
             let f: f32 = match float_str.parse() {
                 Ok(f) => f,
@@ -65,12 +78,15 @@ pub fn build_byte_code(ast: AST) -> Result<InstructionSetBuilder> {
             };
             instructions.put(ExpressionValue::float(f))?;
         }
+        Classification::Access => {
+            process_node(extract_index(node.left, "left", index)?, ast, instructions)?;
+            process_node(extract_index(node.right, "right", index)?, ast, instructions)?;
+            instructions.perform_access();
+        }
         _ => ()
-    }
+    };
 
-    instructions.end_expression();
-
-    return Ok(instructions);
+    Ok(())
 }
 
 #[cfg(test)]
@@ -314,6 +330,38 @@ mod tests {
         let mut expected = InstructionSetBuilder::new();
         expected.start_expression("main");
         expected.resolve(&"my_value".into());
+        expected.end_expression();
+
+        assert_eq!(instructions, expected);
+    }
+
+    #[test]
+    fn access() {
+        let instructions = byte_code_from("my_object.my_value");
+
+        let mut expected = InstructionSetBuilder::new();
+        expected.start_expression("main");
+        expected.resolve(&"my_object".into());
+        expected.resolve(&"my_value".into());
+        expected.perform_access();
+        expected.end_expression();
+
+        assert_eq!(instructions, expected);
+    }
+
+    #[test]
+    fn multi_access() {
+        let instructions = byte_code_from("my_object.my_sub_object.my_property.my_value");
+
+        let mut expected = InstructionSetBuilder::new();
+        expected.start_expression("main");
+        expected.resolve(&"my_object".into());
+        expected.resolve(&"my_sub_object".into());
+        expected.perform_access();
+        expected.resolve(&"my_property".into());
+        expected.perform_access();
+        expected.resolve(&"my_value".into());
+        expected.perform_access();
         expected.end_expression();
 
         assert_eq!(instructions, expected);
