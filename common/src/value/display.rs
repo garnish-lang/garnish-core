9281e6b1fb608c58d6eafc04c477a35fb1a60c3a
Chad Collins <chadrcollins91@gmail.com>
2020-04-23 20:24:49 -0400

    Display for remaining basic values

--- common/src/value/display.rs
+++ common/src/value/display.rs
@@ -7,7 +7,12 @@ impl fmt::Display for ExpressionValueRef<'_> {
         let s = match self.get_type() {
             Err(e) => e.get_message().clone(),
             Ok(t) => match t {
+                DataType::Unit => String::from("()"),
                 DataType::Integer => format!("{}", self.as_integer().unwrap()),
+                DataType::Float => format!("{}", self.as_float().unwrap()),
+                DataType::Character => format!("'{}'", self.as_string().unwrap()),
+                DataType::CharacterList => format!("\"{}\"", self.as_string().unwrap()),
+                DataType::Symbol => format!(":{}", self.as_string().unwrap()),
                 _ => String::new()
             }
         };
@@ -21,9 +26,39 @@ mod tests {
     use std::fmt::Display;
 
     #[test]
+    fn unit() {
+        let value: ExpressionValue = ExpressionValue::unit().into();
+        assert_eq!(format!("{}", value.reference().unwrap()), "()");
+    }
+
+    #[test]
     fn integer() {
         let value: ExpressionValue = ExpressionValue::integer(10).into();
         assert_eq!(format!("{}", value.reference().unwrap()), "10");
+    }
+
+    #[test]
+    fn float() {
+        let value: ExpressionValue = ExpressionValue::float(3.14).into();
+        assert_eq!(format!("{}", value.reference().unwrap()), "3.14");
+    }
+
+    #[test]
+    fn character() {
+        let value: ExpressionValue = ExpressionValue::character('a'.to_string()).into();
+        assert_eq!(format!("{}", value.reference().unwrap()), "'a'");
+    }
+
+    #[test]
+    fn character_list() {
+        let value: ExpressionValue = ExpressionValue::character_list("pandas".into()).into();
+        assert_eq!(format!("{}", value.reference().unwrap()), "\"pandas\"");
+    }
+
+    #[test]
+    fn symbol() {
+        let value: ExpressionValue = ExpressionValue::symbol("my_symbol").into();
+        assert_eq!(format!("{}", value.reference().unwrap()), ":my_symbol");
     }
 }
 
