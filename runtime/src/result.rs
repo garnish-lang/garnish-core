0a0d89714609885a01d48cf92c379c766ee00679
Chad Collins <chadrcollins91@gmail.com>
2021-10-13 20:08:07 -0400

    Created custom result types

---
+++ runtime/src/result.rs
@@ -0,0 +1,48 @@
+pub type GarnishLangRuntimeResult<T=()> = Result<GarnishLangRuntimeData<T>, GarnishLangRuntimeError>;
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct GarnishLangRuntimeData<T> {
+    message: String,
+    data: T
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct GarnishLangRuntimeError {
+    message: String
+}
+
+impl <T> GarnishLangRuntimeData<T> {
+    pub fn new(message: String, data: T) -> Self {
+        return GarnishLangRuntimeData { message, data }
+    }
+
+    pub fn new_from_data(data: T) -> Self {
+        return GarnishLangRuntimeData { message: String::default(), data }
+    }
+
+    pub fn get_message(&self) -> &String {
+        &self.message
+    }
+
+    pub fn get_data(&self) -> &T {
+        &self.data
+    }
+}
+
+impl GarnishLangRuntimeError {
+    pub fn new(message: String) -> Self {
+        return GarnishLangRuntimeError { message }
+    }
+
+    pub fn get_message(&self) -> &String {
+        &self.message
+    }
+}
+
+pub fn result() -> GarnishLangRuntimeData<()> {
+    GarnishLangRuntimeData::new("".to_string(), ())
+}
+
+pub fn error(message: String) -> GarnishLangRuntimeError {
+    GarnishLangRuntimeError::new(message)
+}
