7fe862faf1085f6732991c73fcbdd38e16423d32
Chad Collins <chadrcollins91@gmail.com>
2021-11-22 20:29:29 -0500

    Instructions for multiple items in a list

--- compiler/src/make_instructions.rs
+++ compiler/src/make_instructions.rs
@@ -4,6 +4,7 @@ use log::trace;
 
 use crate::parser::*;
 
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub struct InstructionSet {
     instructions: Vec<InstructionData>,
     data: Vec<ExpressionData>,
@@ -31,6 +32,9 @@ fn add_instructions_for_node(
     nodes: &Vec<ParseNode>,
     instructions: &mut Vec<InstructionData>,
     data: &mut Vec<ExpressionData>,
+    // making_list: &mut bool,
+    list_counts: &mut Vec<usize>,
+    parent_definition: Definition,
 ) -> Result<(), String> {
     Ok(match node_index {
         None => Err(format!(
@@ -42,115 +46,155 @@ fn add_instructions_for_node(
                 "Node at index {:?} does not exist. All nodes should resolve properly if starting from root node.",
                 node_index
             ))?,
-            Some(node) => match node.get_definition() {
-                Definition::Number => {
-                    instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
+            Some(node) => {
+                trace!(
+                    "Visiting node with definition {:?} at {:?} and parent {:?}",
+                    node.get_definition(),
+                    node_index,
+                    parent_definition
+                );
 
-                    data.push(ExpressionData::integer(match node.get_lex_token().get_text().parse::<i64>() {
-                        Err(e) => Err(e.to_string())?,
-                        Ok(i) => i,
-                    }));
+                match list_counts.last_mut() {
+                    None => (),
+                    Some(count) => {
+                        if node.get_definition() != Definition::List {
+                            *count += 1;
+                            trace!("Adding to list, current count {:?}", count);
+                        }
+                    }
                 }
-                Definition::Identifier => {
-                    instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
-                    instructions.push(InstructionData::new(Instruction::Resolve, None));
 
-                    data.push(ExpressionData::symbol_from_string(node.get_lex_token().get_text()));
-                }
-                Definition::Unit => {
-                    // all unit literals will use unit used in the zero element slot of data
-                    instructions.push(InstructionData::new(Instruction::Put, Some(0)));
-                }
-                Definition::Symbol => {
-                    instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
+                match node.get_definition() {
+                    Definition::Number => {
+                        instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
 
-                    data.push(ExpressionData::symbol_from_string(&node.get_lex_token().get_text()[1..].to_string()));
-                }
-                Definition::Input => {
-                    // all unit literals will use unit used in the zero element slot of data
-                    instructions.push(InstructionData::new(Instruction::PutInput, None));
-                }
-                Definition::Result => {
-                    // all unit literals will use unit used in the zero element slot of data
-                    instructions.push(InstructionData::new(Instruction::PutResult, None));
-                }
-                Definition::AbsoluteValue => todo!(), // not currently in runtime
-                Definition::EmptyApply => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
+                        data.push(ExpressionData::integer(match node.get_lex_token().get_text().parse::<i64>() {
+                            Err(e) => Err(e.to_string())?,
+                            Ok(i) => i,
+                        }));
+                    }
+                    Definition::Identifier => {
+                        instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
+                        instructions.push(InstructionData::new(Instruction::Resolve, None));
 
-                    instructions.push(InstructionData::new(Instruction::Put, Some(0)));
-                    instructions.push(InstructionData::new(Instruction::Apply, None));
-                }
-                Definition::Addition => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        data.push(ExpressionData::symbol_from_string(node.get_lex_token().get_text()));
+                    }
+                    Definition::Unit => {
+                        // all unit literals will use unit used in the zero element slot of data
+                        instructions.push(InstructionData::new(Instruction::Put, Some(0)));
+                    }
+                    Definition::Symbol => {
+                        instructions.push(InstructionData::new(Instruction::Put, Some(data.len())));
 
-                    instructions.push(InstructionData::new(Instruction::PerformAddition, None));
-                }
-                Definition::Equality => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        data.push(ExpressionData::symbol_from_string(&node.get_lex_token().get_text()[1..].to_string()));
+                    }
+                    Definition::Input => {
+                        // all unit literals will use unit used in the zero element slot of data
+                        instructions.push(InstructionData::new(Instruction::PutInput, None));
+                    }
+                    Definition::Result => {
+                        // all unit literals will use unit used in the zero element slot of data
+                        instructions.push(InstructionData::new(Instruction::PutResult, None));
+                    }
+                    Definition::AbsoluteValue => todo!(), // not currently in runtime
+                    Definition::EmptyApply => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::EqualityComparison, None));
-                }
-                Definition::Pair => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        instructions.push(InstructionData::new(Instruction::Put, Some(0)));
+                        instructions.push(InstructionData::new(Instruction::Apply, None));
+                    }
+                    Definition::Addition => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::MakePair, None));
-                }
-                Definition::Access => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        instructions.push(InstructionData::new(Instruction::PerformAddition, None));
+                    }
+                    Definition::Equality => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::Access, None));
-                }
-                Definition::List => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        instructions.push(InstructionData::new(Instruction::EqualityComparison, None));
+                    }
+                    Definition::Pair => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::MakeList, Some(2)));
-                }
-                Definition::Subexpression => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
+                        instructions.push(InstructionData::new(Instruction::MakePair, None));
+                    }
+                    Definition::Access => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::PushResult, None));
+                        instructions.push(InstructionData::new(Instruction::Access, None));
+                    }
+                    Definition::List => {
+                        list_counts.push(0);
 
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
-                }
-                Definition::Group => todo!(),
-                Definition::NestedExpression => todo!(),
-                Definition::Apply => {
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::Apply, None));
-                }
-                Definition::ApplyTo => {
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
-                    add_instructions_for_node(node.get_left(), nodes, instructions, data)?;
+                        if parent_definition != Definition::List {
+                            instructions.push(InstructionData::new(
+                                Instruction::MakeList,
+                                Some(match list_counts.pop() {
+                                    None => Err(format!("Count not get list count out of array."))?,
+                                    Some(i) => i,
+                                }),
+                            ));
+                        }
+                    }
+                    Definition::Subexpression => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
 
-                    instructions.push(InstructionData::new(Instruction::Apply, None));
-                }
-                Definition::Reapply => {
-                    add_instructions_for_node(node.get_right(), nodes, instructions, data)?;
+                        instructions.push(InstructionData::new(Instruction::PushResult, None));
 
-                    instructions.push(InstructionData::new(Instruction::Reapply, None));
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
+                    }
+                    Definition::Group => todo!(),
+                    Definition::NestedExpression => todo!(),
+                    Definition::Apply => {
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
+
+                        instructions.push(InstructionData::new(Instruction::Apply, None));
+                    }
+                    Definition::ApplyTo => {
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
+                        add_instructions_for_node(node.get_left(), nodes, instructions, data, list_counts, node.get_definition())?;
+
+                        instructions.push(InstructionData::new(Instruction::Apply, None));
+                    }
+                    Definition::Reapply => {
+                        add_instructions_for_node(node.get_right(), nodes, instructions, data, list_counts, node.get_definition())?;
+
+                        instructions.push(InstructionData::new(Instruction::Reapply, None));
+                    }
+                    Definition::ApplyIfTrue => todo!(),
+                    Definition::ApplyIfFalse => todo!(),
+                    Definition::ConditionalBranch => todo!(),
+                    // no runtime meaning, parser only utility
+                    Definition::Drop => (),
                 }
-                Definition::ApplyIfTrue => todo!(),
-                Definition::ApplyIfFalse => todo!(),
-                Definition::ConditionalBranch => todo!(),
-                // no runtime meaning, parser only utility
-                Definition::Drop => (),
-            },
+
+                trace!("{:?} at {:?} resolved", node.get_definition(), node_index);
+            }
         },
     })
 }
 
 pub fn instructions_from_ast(root: usize, nodes: Vec<ParseNode>) -> Result<InstructionSet, String> {
     let mut instruction_set = InstructionSet::new();
+    let mut list_counts = vec![];
+    // let mut making_list =
 
-    add_instructions_for_node(Some(root), &nodes, &mut instruction_set.instructions, &mut instruction_set.data)?;
+    add_instructions_for_node(
+        Some(root),
+        &nodes,
+        &mut instruction_set.instructions,
+        &mut instruction_set.data,
+        &mut list_counts,
+        Definition::Drop, // Initial value shouln't matter
+    )?;
 
     instruction_set.instructions.push(InstructionData::new(Instruction::EndExpression, None));
 
@@ -497,6 +541,36 @@ mod lists {
                 (Instruction::EndExpression, None),
             ],
             vec![ExpressionData::integer(5), ExpressionData::integer(10)],
+        );
+    }
+
+    #[test]
+    fn multiple_items_in_list() {
+        assert_instruction_data(
+            5,
+            vec![
+                (Definition::Number, Some(1), None, None, "5", TokenType::Number),
+                (Definition::List, Some(3), Some(0), Some(2), ",", TokenType::Comma),
+                (Definition::Number, Some(1), None, None, "10", TokenType::Number),
+                (Definition::List, Some(5), Some(1), Some(4), ",", TokenType::Comma),
+                (Definition::Number, Some(3), None, None, "15", TokenType::Number),
+                (Definition::List, None, Some(3), Some(6), ",", TokenType::Comma),
+                (Definition::Number, Some(5), None, None, "20", TokenType::Number),
+            ],
+            vec![
+                (Instruction::Put, Some(1)),
+                (Instruction::Put, Some(2)),
+                (Instruction::Put, Some(3)),
+                (Instruction::Put, Some(4)),
+                (Instruction::MakeList, Some(4)),
+                (Instruction::EndExpression, None),
+            ],
+            vec![
+                ExpressionData::integer(5),
+                ExpressionData::integer(10),
+                ExpressionData::integer(15),
+                ExpressionData::integer(20),
+            ],
         );
     }
 }
