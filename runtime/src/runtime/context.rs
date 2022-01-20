0fe542611f5ff76d1921818fe544175b63e0c226
Chad Collins <chadrcollins91@gmail.com>
2022-01-20 18:26:39 -0500

    Moved EmptyContext impls to be default impls

--- runtime/src/runtime/context.rs
+++ runtime/src/runtime/context.rs
@@ -4,21 +4,15 @@ pub trait GarnishLangRuntimeContext<Data>
 where
     Data: GarnishLangRuntimeData,
 {
-    fn resolve(&mut self, symbol: Data::Symbol, runtime: &mut Data) -> Result<bool, RuntimeError<Data::Error>>;
-    fn apply(&mut self, external_value: Data::Size, input_addr: Data::Size, runtime: &mut Data) -> Result<bool, RuntimeError<Data::Error>>;
+    fn resolve(&mut self, _symbol: Data::Symbol, _runtime: &mut Data) -> Result<bool, RuntimeError<Data::Error>> {
+        Ok(false)
+    }
+
+    fn apply(&mut self, _external_value: Data::Size, _input_addr: Data::Size, _runtime: &mut Data) -> Result<bool, RuntimeError<Data::Error>> {
+        Ok(false)
+    }
 }
 
 pub struct EmptyContext {}
 
-impl<Data> GarnishLangRuntimeContext<Data> for EmptyContext
-where
-    Data: GarnishLangRuntimeData,
-{
-    fn resolve(&mut self, _: Data::Symbol, _: &mut Data) -> Result<bool, RuntimeError<Data::Error>> {
-        Ok(false)
-    }
-
-    fn apply(&mut self, _: Data::Size, _: Data::Size, _: &mut Data) -> Result<bool, RuntimeError<Data::Error>> {
-        Ok(false)
-    }
-}
+impl<Data> GarnishLangRuntimeContext<Data> for EmptyContext where Data: GarnishLangRuntimeData {}
