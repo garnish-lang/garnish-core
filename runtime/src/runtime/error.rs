4705674288939a9b791442054f260424e9e2f704
Chad Collins <chadrcollins91@gmail.com>
2022-05-28 14:43:58 -0400

    Swapped in error from traits

--- runtime/src/runtime/error.rs
+++ runtime/src/runtime/error.rs
@@ -1,86 +1,9 @@
-use crate::{Instruction};
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
-
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub enum ErrorType {
-    Unknown,
-    // special code used internally to defer error during complex matching
-    UnsupportedOpTypes,
-}
+use crate::Instruction;
+pub use garnish_traits::{ErrorType, RuntimeError};
+use std::fmt::Debug;
 
 pub trait OrNumberError<T, Source: 'static + std::error::Error> {
     fn or_num_err(self) -> Result<T, RuntimeError<Source>>;
-}
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct RuntimeError<Source: 'static + std::error::Error> {
-    code: ErrorType,
-    message: String,
-    source: Option<Source>,
-}
-
-impl<Source: 'static + std::error::Error> RuntimeError<Source> {
-    pub fn new(message: &str) -> Self {
-        RuntimeError {
-            code: ErrorType::Unknown,
-            message: message.to_string(),
-            source: None,
-        }
-    }
-
-    pub fn new_message(message: String) -> Self {
-        RuntimeError {
-            code: ErrorType::Unknown,
-            message,
-            source: None,
-        }
-    }
-
-    pub fn unsupported_types() -> Self {
-        RuntimeError {
-            code: ErrorType::UnsupportedOpTypes,
-            message: String::new(),
-            source: None,
-        }
-    }
-
-    pub fn get_type(&self) -> ErrorType {
-        self.code
-    }
-}
-
-impl<Source: 'static + std::error::Error> Default for RuntimeError<Source> {
-    fn default() -> Self {
-        RuntimeError {
-            code: ErrorType::Unknown,
-            message: String::new(),
-            source: None,
-        }
-    }
-}
-
-impl<Source: 'static + std::error::Error> Display for RuntimeError<Source> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(format!("{:?}", self).as_str())
-    }
-}
-
-impl<Source: 'static + std::error::Error> std::error::Error for RuntimeError<Source> {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match &self.source {
-            None => None,
-            Some(s) => Some(s),
-        }
-    }
-}
-
-impl<Source: 'static + std::error::Error> From<Source> for RuntimeError<Source> {
-    fn from(source: Source) -> Self {
-        let mut e = RuntimeError::default();
-        e.source = Some(source);
-        e
-    }
 }
 
 // impl<Num: GarnishNumber, Source: 'static + std::error::Error> OrNumberError<Num, Source> for Option<Num> {
