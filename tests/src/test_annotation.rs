9f18cf9b7436405756b72b96689968658973e5a3
Chad Collins <chadrcollins91@gmail.com>
2022-05-30 13:25:39 -0400

    Implemented first test for test annotation processing

--- tests/src/test_annotation.rs
+++ tests/src/test_annotation.rs
@@ -1,4 +1,8 @@
-use garnish_lang_compiler::LexerToken;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::iter;
+
+use garnish_lang_compiler::{LexerToken, TokenType};
 
 /// Test Annotations
 ///
@@ -25,28 +29,145 @@ pub struct TestAnnotationDetails {
     expression: Vec<LexerToken>,
 }
 
+impl TestAnnotationDetails {
+    fn new(name: String, expression: Vec<LexerToken>) -> Self {
+        TestAnnotationDetails { name, expression }
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn get_expression(&self) -> &Vec<LexerToken> {
+        &self.expression
+    }
+}
+
 pub struct TestDetails {
-    tests: Vec<TestAnnotationDetails>
+    tests: Vec<TestAnnotationDetails>,
 }
 
 impl TestDetails {
+    fn new() -> Self {
+        TestDetails { tests: Vec::new() }
+    }
+
     pub fn get_tests(&self) -> &Vec<TestAnnotationDetails> {
         &self.tests
     }
 }
 
+#[derive(Debug)]
+pub struct TestExtractionError {
+    reason: String,
+}
 
+impl TestExtractionError {
+    pub fn error(s: &str) -> Self {
+        TestExtractionError { reason: s.to_string() }
+    }
+}
+
+impl Display for TestExtractionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.reason.as_str())
+    }
+}
+
+impl Error for TestExtractionError {}
+
+enum ExtractionState {
+    Searching,
+    InTest,
+}
+
+fn get_first_non_space(tokens: &Vec<LexerToken>, start: usize) -> (usize, LexerToken) {
+    let unknown_token= LexerToken::new("".to_string(), TokenType::Unknown, 0, 0);
+    let mut non_space = (0, unknown_token);
+    for (i, token) in tokens.iter().enumerate().skip(start) {
+        match token.get_token_type() {
+            TokenType::Whitespace => continue,
+            _ => {
+                non_space = (i, token.clone());
+                break;
+            }
+        }
+    }
+
+    non_space
+}
+
+pub fn extract_tests(tokens: &Vec<LexerToken>) -> Result<TestDetails, TestExtractionError> {
+    let mut extraction_details = TestDetails::new();
+    let mut state = ExtractionState::Searching;
+    let unknown_token= LexerToken::new("".to_string(), TokenType::Unknown, 0, 0);
+    let mut iter = tokens.iter().chain(iter::once(&unknown_token));
+
+    let mut current_extraction = Vec::new();
+
+    while let Some(next) = iter.next() {
+        match state {
+            ExtractionState::Searching => {
+                match next.get_token_type() {
+                    TokenType::Annotation => match next.get_text().as_str() {
+                        "@Test" => {
+                            state = ExtractionState::InTest;
+                        }
+                        _ => (), // none test annotation
+                    },
+                    _ => (), // go to next token
+                }
+            }
+            ExtractionState::InTest => {
+                // get all tokens until first un-nested Subexpression token
+                match next.get_token_type() {
+                    TokenType::Unknown => {
+                        // finalize test annotation details
+
+                        // first non space token should be a string for name
+                        let non_space = get_first_non_space(&current_extraction, 0);
+
+                        // String tokens include the quote marks, trim here
+                        let name = non_space.1.get_text().trim_matches('"').to_string();
+
+                        // next non-space token after name should be start of expression
+                        let expression_start = get_first_non_space(&current_extraction, non_space.0 + 1);
+
+                        // create details
+                        let expression = Vec::from(&current_extraction[expression_start.0..]);
+                        let details = TestAnnotationDetails::new(name, expression);
+                        extraction_details.tests.push(details);
+
+                        // reset
+                        current_extraction = Vec::new();
+                        state = ExtractionState::Searching;
+                    }
+                    _ => {
+                        current_extraction.push(next.clone());
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(extraction_details)
+}
 
 #[cfg(test)]
 mod tests {
     use garnish_lang_compiler::lex;
 
+    use crate::test_annotation::extract_tests;
+
     #[test]
     fn gathers_all_parts() {
         let tokens = lex("@Test \"Plus 10\" { 5 + 10 == 15 }").unwrap();
 
-        let test_details = extract_tests(&tokens);
+        let test_details = extract_tests(&tokens).unwrap();
 
         assert_eq!(test_details.get_tests().len(), 1);
+        let detail = test_details.get_tests().get(0).unwrap();
+        assert_eq!(detail.get_name(), &"Plus 10".to_string());
+        assert_eq!(detail.get_expression().len(), 13);
     }
 }
