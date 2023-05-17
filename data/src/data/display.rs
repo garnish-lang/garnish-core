733fad95f7dd67d746b481f16a0f6314f1d9b69f
Chad Collins <chadrcollins91@gmail.com>
2023-05-17 18:32:31 -0400

    Began display of simple data list

--- data/src/data/display.rs
+++ data/src/data/display.rs
@@ -1,12 +1,16 @@
-use crate::data::SimpleData;
 use std::fmt::{Debug, Display};
 use std::hash::Hash;
+
+use crate::data::{SimpleData, SimpleDataList};
 
 impl<T> SimpleData<T>
 where
     T: Clone + Copy + PartialEq + Eq + PartialOrd + Debug + Hash,
 {
-    pub fn display_simple(&self) -> String where T: Display {
+    pub fn display_simple(&self) -> String
+    where
+        T: Display,
+    {
         match self {
             SimpleData::Unit => "()".into(),
             SimpleData::True => "True".into(),
@@ -34,12 +38,58 @@ where
     }
 }
 
+impl<T> SimpleDataList<T>
+where
+    T: Clone + Copy + PartialEq + Eq + PartialOrd + Debug + Hash,
+{
+    pub fn display_for_item(&self, index: usize) -> Option<String>
+    where
+        T: Display,
+    {
+        self.display_for_item_internal(index, 0)
+    }
+
+    fn display_for_item_internal(&self, index: usize, level: usize) -> Option<String>
+    where
+        T: Display,
+    {
+        match self.get(index) {
+            None => None,
+            Some(item) => Some(match item {
+                SimpleData::ByteList(bytes) => match level > 0 {
+                    true => format!("({})", bytes.iter().map(|b| b.to_string()).collect::<Vec<String>>().join(" ")),
+                    false => format!("{}", bytes.iter().map(|b| b.to_string()).collect::<Vec<String>>().join(" ")),
+                },
+                SimpleData::List(items, _) => items
+                    .iter()
+                    .map(|item| self.display_for_item_internal(*item, level + 1).or(Some("<NoData>".to_string())).unwrap())
+                    .collect::<Vec<String>>()
+                    .join(", "),
+                _ => item.display_simple(),
+            }),
+        }
+    }
+}
+
 #[cfg(test)]
-mod simple {
+mod shared {
     use std::fmt::{Display, Formatter};
 
+    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Debug, Hash)]
+    pub struct StructWith {}
+
+    impl Display for StructWith {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            f.write_str("StructWith")
+        }
+    }
+}
+
+#[cfg(test)]
+mod simple {
     use garnish_traits::ExpressionDataType;
 
+    use crate::data::display::shared::StructWith;
     use crate::data::{SimpleData, SimpleNumber};
     use crate::NoCustom;
 
@@ -139,18 +189,49 @@ mod simple {
         assert_eq!(data.display_simple(), "List([10, 20], [20, 10])".to_string());
     }
 
-    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Debug, Hash)]
-    struct StructWith {}
-
-    impl Display for StructWith {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            f.write_str("StructWith")
-        }
-    }
-
     #[test]
     fn simple_custom() {
         let data: SimpleData<StructWith> = SimpleData::Custom(StructWith {});
         assert_eq!(data.display_simple(), "StructWith".to_string());
+    }
+}
+
+#[cfg(test)]
+mod simple_list {
+    use garnish_traits::ExpressionDataType;
+
+    use crate::data::display::shared::StructWith;
+    use crate::data::{SimpleData, SimpleDataList, SimpleNumber};
+
+    #[test]
+    fn non_existent_item_is_none() {
+        let list: SimpleDataList<StructWith> = SimpleDataList::new();
+
+        assert!(list.display_for_item(10).is_none())
+    }
+
+    #[test]
+    fn list_of_items() {
+        let mut list: SimpleDataList<StructWith> = SimpleDataList::new();
+        list.push(SimpleData::Unit);
+        list.push(SimpleData::True);
+        list.push(SimpleData::False);
+        list.push(SimpleData::Type(ExpressionDataType::Byte));
+        list.push(SimpleData::Number(SimpleNumber::Integer(100)));
+        list.push(SimpleData::Char('c'));
+        list.push(SimpleData::Byte(10));
+        list.push(SimpleData::Symbol(100));
+        list.push(SimpleData::Expression(100));
+        list.push(SimpleData::External(100));
+        list.push(SimpleData::CharList("test".to_string()));
+        list.push(SimpleData::ByteList(vec![10, 20]));
+        list.push(SimpleData::Custom(StructWith {}));
+
+        list.push(SimpleData::List(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15], vec![]));
+
+        assert_eq!(
+            list.display_for_item(13).unwrap(),
+            "(), True, False, Byte, 100, c, 10, Symbol(100), Expression(100), External(100), test, (10 20), StructWith, <NoData>"
+        );
     }
 }
