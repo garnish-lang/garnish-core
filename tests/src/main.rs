6a1e481a025f11e5d658a85ab552b219765b714d
chadrc <chadrcollins91@gmail.com>
2025-03-26 20:24:13 -0400

    Added more test scripts, collecting success and failure counts

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -44,89 +44,130 @@ fn main() {
     env_logger::init();
 
     let script_paths = collect_scripts(String::from("tests/scripts"));
+    let mut successes = 0;
+    let mut failures = 0;
+    let mut messages = vec![];
 
     for script_path in script_paths {
-        let mut data = SimpleGarnishData::new();
-        match read_to_string(PathBuf::from(&script_path))
-            .or_else(|e| Err(format!("{}", e)))
-            .and_then(|file| {
-                lex(&file)
-                    .or_else(|e| Err(format!("{}", e)))
-                    .and_then(|tokens| parse(&tokens).or_else(|e| Err(format!("{}", e))))
-                    .and_then(|parse_result| {
-                        build_with_data(parse_result.get_root(), parse_result.get_nodes().clone(), &mut data).or_else(|e| Err(format!("{}", e)))
-                    })
-                    .or_else(|e| Err(format!("{}", e)))
-            }) {
-            Err(e) => error!("Test failed: {}", e),
-            Ok(_) => {
-                let start = match data.get_jump_points().get(0) {
-                    Some(jump_points) => *jump_points,
-                    None => {
-                        error!("Test failed: No jump point");
-                        return;
-                    }
-                };
+        let result = execute_script(&script_path);
 
-                match data.set_instruction_cursor(start) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        error!("Test failed: {}", e);
-                        return;
-                    }
+        let message = match result {
+            TestResult::Success => {
+                successes += 1;
+                "success".green()
+            },
+            TestResult::Failure(s) => {
+                failures += 1;
+                format!("failure - [{}]", s).red()
+            },
+            TestResult::Error(s) => {
+                failures += 1;
+                format!("error - {}", s).yellow()
+            }
+        };
+
+        let mut path = PathBuf::from(&script_path);
+        while let Some(_) = path.extension() {
+            path.set_extension("");
+        }
+
+        let script_name = path.to_string_lossy()
+            .replace("tests/scripts", "")
+            .trim_matches(path::MAIN_SEPARATOR)
+            .replace(path::MAIN_SEPARATOR, ":");
+
+        messages.push(format!("{} - {}", script_name, message));
+    }
+
+    println!("{} | {}", format!("{} successes", successes).green(), format!("{} failures", failures).red());
+    for m in messages {
+        println!("{}", m);
+    }
+}
+
+enum TestResult {
+    Success,
+    Failure(String),
+    Error(String)
+}
+
+fn execute_script(script_path: &String) -> TestResult {
+    let mut data = SimpleGarnishData::new();
+    match read_to_string(PathBuf::from(&script_path))
+        .or_else(|e| Err(format!("{}", e)))
+        .and_then(|file| {
+            lex(&file)
+                .or_else(|e| Err(format!("{}", e)))
+                .and_then(|tokens| parse(&tokens).or_else(|e| Err(format!("{}", e))))
+                .and_then(|parse_result| {
+                    build_with_data(parse_result.get_root(), parse_result.get_nodes().clone(), &mut data).or_else(|e| Err(format!("{}", e)))
+                })
+                .or_else(|e| Err(format!("{}", e)))
+        }) {
+        Err(e) => TestResult::Error(format!("({}): {}", &script_path, e)),
+        Ok(_) => {
+            let start = match data.get_jump_points().get(0) {
+                Some(jump_points) => *jump_points,
+                None => {
+                    return TestResult::Error(format!("({}) No jump point", &script_path));
                 }
+            };
 
-                match data.add_unit().and_then(|e| data.push_value_stack(e)) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        error!("Test failed: {}", e);
-                        return;
-                    }
+            match data.set_instruction_cursor(start) {
+                Ok(_) => (),
+                Err(e) => {
+                    return TestResult::Error(format!("({}): {}", &script_path, e));
                 }
+            }
 
-                let mut context = TestingContext::default();
-                let mut runtime = SimpleGarnishRuntime::new(data);
+            match data.add_unit().and_then(|e| data.push_value_stack(e)) {
+                Ok(_) => (),
+                Err(e) => {
+                    return TestResult::Error(format!("({}): {}", &script_path, e));
+                }
+            }
 
-                loop {
-                    match runtime.execute_current_instruction(Some(&mut context)) {
-                        Err(e) => {
-                            error!("Test failed: {}", e);
-                            return;
+            let mut context = TestingContext::default();
+            let mut runtime = SimpleGarnishRuntime::new(data);
+
+            loop {
+                match runtime.execute_current_instruction(Some(&mut context)) {
+                    Err(e) => {
+                        return TestResult::Error(format!("({}): {}", &script_path, e));
+                    }
+                    Ok(data) => match data.get_state() {
+                        SimpleRuntimeState::Running => (),
+                        SimpleRuntimeState::End => break,
+                    },
+                }
+            }
+
+            let data = runtime.get_data_owned();
+
+            let result = match data.get_current_value().and_then(|i| data.get_data().get(i)) {
+                Some(value) => value,
+                None => {
+                    return TestResult::Error(format!("({}) No current value after execution", &script_path));
+                }
+            };
+
+            match result {
+                SimpleData::Pair(left_index, right_index) => {
+                    match (data.get_data().get(*left_index), data.get_data().get(*right_index)) {
+                        (Some(left), Some(right)) => {
+                            if left == right {
+                                TestResult::Success
+                            } else {
+                                TestResult::Failure(format!("failed [{} = {}]",
+                                        data.get_data().display_for_item(*left_index),
+                                        data.get_data().display_for_item(*right_index)
+                                ))
+                            }
                         }
-                        Ok(data) => match data.get_state() {
-                            SimpleRuntimeState::Running => (),
-                            SimpleRuntimeState::End => break,
-                        },
+                        (l, r) => TestResult::Error(format!("invalid Pair value. left = {:?}, right = {:?}", l, r))
                     }
                 }
-
-                let data = runtime.get_data_owned();
-
-                let result = match data.get_current_value().and_then(|i| data.get_data().get(i)) {
-                    Some(value) => value,
-                    None => {
-                        error!("Test failed: No current value after execution");
-                        return;
-                    }
-                };
-
-                let message = match result {
-                    SimpleData::True => "passed".green(),
-                    SimpleData::False => "failed".red(),
-                    t => format!("Expected a True or False value, got {:?}", t).yellow(),
-                };
-
-                let mut path = PathBuf::from(&script_path);
-                while let Some(_) = path.extension() {
-                    path.set_extension("");
-                }
-
-                let script_name = path.to_string_lossy()
-                    .replace("tests/scripts", "")
-                    .trim_matches(path::MAIN_SEPARATOR)
-                    .replace(path::MAIN_SEPARATOR, ":");
-
-                println!("{} - {}", script_name, message)
+                t => TestResult::Error(format!("expected a Pair value, got {:?}", t)),
             }
         }
     }
