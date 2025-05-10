56671f3b94afd62e754cb4e3a35d597fd8e74e84
chadrc <chadrcollins91@gmail.com>
2025-05-10 13:49:56 -0400

    Retaining single value

--- data/src/optimize.rs
+++ data/src/optimize.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use crate::{DataError, SimpleDataList, SimpleGarnishData};
@@ -7,8 +8,19 @@ where
     T: Clone + PartialEq + Eq + PartialOrd + Debug + Hash,
 {
     pub fn optimize(&mut self, retain_data: Vec<usize>) -> Result<(), DataError> {
+        let old_data = self.data.clone();
         self.data = SimpleDataList::default();
+        self.cache = HashMap::new();
         
+        for data in retain_data {
+            match old_data.get(data) {
+                None => Err(DataError::from(format!("No data at {} while optimizing", data)))?,
+                Some(value) => {
+                    self.cache_add(value.clone())?;
+                }
+            }
+        }
+
         Ok(())
     }
 }
@@ -16,7 +28,13 @@ where
 #[cfg(test)]
 mod tests {
     use garnish_lang_traits::GarnishData;
-    use crate::{SimpleGarnishData, SimpleNumber};
+    use crate::{SimpleData, SimpleGarnishData, SimpleNumber};
+
+    fn assert_default_data(data: &SimpleGarnishData) {
+        assert_eq!(data.data.get(0).unwrap(), &SimpleData::Unit);
+        assert_eq!(data.data.get(1).unwrap(), &SimpleData::False);
+        assert_eq!(data.data.get(2).unwrap(), &SimpleData::True);
+    }
 
     #[test]
     fn optimize_with_no_retention() {
@@ -24,9 +42,23 @@ mod tests {
         data.add_number(SimpleNumber::Integer(100)).unwrap();
         data.add_number(SimpleNumber::Integer(200)).unwrap();
         data.add_number(SimpleNumber::Integer(300)).unwrap();
-        
+
         data.optimize(vec![]).unwrap();
-        
+
         assert_eq!(data.get_data().len(), 3);
+        assert_default_data(&data);
+    }
+
+    #[test]
+    fn retain_one_number() {
+        let mut data = SimpleGarnishData::new();
+        data.add_number(SimpleNumber::Integer(100)).unwrap();
+        data.add_number(SimpleNumber::Integer(200)).unwrap();
+        data.add_number(SimpleNumber::Integer(300)).unwrap();
+
+        data.optimize(vec![5]).unwrap();
+
+        assert_default_data(&data);
+        assert_eq!(data.get_data().get(3).unwrap(), &SimpleData::Number(300.into()));
     }
 }
