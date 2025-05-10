1857a1229c7643af839a5d7e197ba306b4fb71d4
chadrc <chadrcollins91@gmail.com>
2025-05-10 14:09:12 -0400

    Retaining a pair

--- data/src/optimize.rs
+++ data/src/optimize.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
-use crate::{DataError, SimpleDataList, SimpleGarnishData};
+use garnish_lang_traits::GarnishData;
+use crate::{DataError, SimpleData, SimpleDataList, SimpleGarnishData};
 
 impl<T> SimpleGarnishData<T>
 where
@@ -9,26 +10,37 @@ where
 {
     pub fn optimize(&mut self, retain_data: Vec<usize>) -> Result<(), DataError> {
         let old_data = self.data.clone();
-        self.data = SimpleDataList::default();
+        self.data = SimpleDataList::<T>::default();
         self.cache = HashMap::new();
         
         for data in retain_data {
-            match old_data.get(data) {
-                None => Err(DataError::from(format!("No data at {} while optimizing", data)))?,
-                Some(value) => {
-                    self.cache_add(value.clone())?;
-                }
-            }
+            self.optimize_retain_item(data, &old_data)?;
         }
 
         Ok(())
+    }
+    
+    fn optimize_retain_item(&mut self, index: usize, reference_data: &SimpleDataList<T>) -> Result<usize, DataError> {
+        match reference_data.get(index) {
+            None => Err(DataError::from(format!("No data at {} while optimizing", index)))?,
+            Some(value) => match value {
+                SimpleData::Pair(left, right) => {
+                    let new_left = self.optimize_retain_item(left.clone(), reference_data)?;
+                    let new_right = self.optimize_retain_item(right.clone(), reference_data)?;
+                    self.add_pair((new_left, new_right))
+                }
+                d => {
+                    self.cache_add(d.clone())
+                }
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use garnish_lang_traits::GarnishData;
-    use crate::{SimpleData, SimpleGarnishData, SimpleNumber};
+    use crate::{NoCustom, SimpleData, SimpleGarnishData, SimpleNumber};
 
     fn assert_default_data(data: &SimpleGarnishData) {
         assert_eq!(data.data.get(0).unwrap(), &SimpleData::Unit);
@@ -60,5 +72,23 @@ mod tests {
 
         assert_default_data(&data);
         assert_eq!(data.get_data().get(3).unwrap(), &SimpleData::Number(300.into()));
+    }
+
+    #[test]
+    fn retain_pair() {
+        let mut data = SimpleGarnishData::new();
+        data.add_number(SimpleNumber::Integer(100)).unwrap();
+        data.add_number(SimpleNumber::Integer(200)).unwrap();
+        let num = data.add_number(SimpleNumber::Integer(300)).unwrap();
+        let sym_data = SimpleGarnishData::<NoCustom>::parse_symbol("number").unwrap();
+        let sym = data.add_symbol(sym_data).unwrap();
+        let pair = data.add_pair((sym, num)).unwrap();
+
+        data.optimize(vec![pair]).unwrap();
+
+        assert_default_data(&data);
+        assert_eq!(data.get_data().get(3).unwrap(), &SimpleData::Symbol(sym_data));
+        assert_eq!(data.get_data().get(4).unwrap(), &SimpleData::Number(300.into()));
+        assert_eq!(data.get_data().get(5).unwrap(), &SimpleData::Pair(3, 4));
     }
 }
