f1704eac2a37db31765b2017e44a4c02913dd54b
Chad Collins <chadrcollins91@gmail.com>
2025-12-13 21:04:33 -0500

    Added utility methods for adding char list and byte list

--- data/src/basic/mod.rs
+++ data/src/basic/mod.rs
@@ -196,6 +196,22 @@ where T: BasicDataCustom {
             None => Err(DataError::new("Invalid data index", DataErrorType::InvalidDataIndex(index))),
         }
     }
+
+    pub fn add_char_list_from_string(&mut self, s: impl AsRef<str>) -> Result<usize, DataError> {
+        let index = self.push_basic_data(BasicData::CharList(s.as_ref().len()));
+        for c in s.as_ref().chars() {
+            self.push_basic_data(BasicData::Char(c));
+        }
+        Ok(index)
+    }
+
+    pub fn add_byte_list_from_vec(&mut self, v: impl AsRef<[u8]>) -> Result<usize, DataError> {
+        let index = self.push_basic_data(BasicData::ByteList(v.as_ref().len()));
+        for b in v.as_ref() {
+            self.push_basic_data(BasicData::Byte(*b));
+        }
+        Ok(index)
+    }
 }
 
 #[cfg(test)]
@@ -254,6 +270,109 @@ mod tests {
         data.push_basic_data(BasicData::Number(100.into()));
         let result = data.get_data_ensure_index(1);
         assert_eq!(result, Err(DataError::new("Invalid data index", DataErrorType::InvalidDataIndex(1))));
+    }
+
+    #[test]
+    fn add_char_list_from_string() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_char_list_from_string("hello").unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(
+            data.data,
+            vec![
+                BasicData::CharList(5),
+                BasicData::Char('h'),
+                BasicData::Char('e'),
+                BasicData::Char('l'),
+                BasicData::Char('l'),
+                BasicData::Char('o'),
+            ]
+        );
+    }
+
+    #[test]
+    fn add_char_list_from_string_empty() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_char_list_from_string("").unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(data.data, vec![BasicData::CharList(0)]);
+    }
+
+    #[test]
+    fn add_char_list_from_string_single_char() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_char_list_from_string("a").unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(
+            data.data,
+            vec![
+                BasicData::CharList(1),
+                BasicData::Char('a'),
+            ]
+        );
+    }
+
+    #[test]
+    fn add_byte_list_from_vec() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_byte_list_from_vec(vec![100, 150, 200]).unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(
+            data.data,
+            vec![
+                BasicData::ByteList(3),
+                BasicData::Byte(100),
+                BasicData::Byte(150),
+                BasicData::Byte(200),
+            ]
+        );
+    }
+
+    #[test]
+    fn add_byte_list_from_vec_empty() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_byte_list_from_vec(vec![]).unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(data.data, vec![BasicData::ByteList(0)]);
+    }
+
+    #[test]
+    fn add_byte_list_from_vec_single_byte() {
+        let mut data = BasicGarnishDataUnit::new();
+        let index = data.add_byte_list_from_vec(vec![42]).unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(
+            data.data,
+            vec![
+                BasicData::ByteList(1),
+                BasicData::Byte(42),
+            ]
+        );
+    }
+
+    #[test]
+    fn add_byte_list_from_vec_slice() {
+        let mut data = BasicGarnishDataUnit::new();
+        let bytes: [u8; 4] = [10, 20, 30, 40];
+        let index = data.add_byte_list_from_vec(&bytes[..]).unwrap();
+
+        assert_eq!(index, 0);
+        assert_eq!(
+            data.data,
+            vec![
+                BasicData::ByteList(4),
+                BasicData::Byte(10),
+                BasicData::Byte(20),
+                BasicData::Byte(30),
+                BasicData::Byte(40),
+            ]
+        );
     }
 }
 
