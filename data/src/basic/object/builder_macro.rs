3a1691e03ac9b7bcba12bef733ed5c4e49b948f4
Chad Collins <chadrcollins91@gmail.com>
2025-12-21 20:18:36 -0500

    Pairs in builder macro

--- data/src/basic/object/builder_macro.rs
+++ data/src/basic/object/builder_macro.rs
@@ -1,5 +1,11 @@
 #[macro_export]
 macro_rules! basic_object {
+    (($($inner:tt)+)) => {
+        basic_object!($($inner)+)
+    };
+    ($left:tt = $right:tt) => {
+        BasicObject::Pair(Box::new(basic_object!($left)), Box::new(basic_object!($right)))
+    };
     (Unit) => {
         BasicObject::Unit
     };
@@ -9,19 +15,19 @@ macro_rules! basic_object {
     (False) => {
         BasicObject::False
     };
-    (Type($garnish_type:ident)) => {
+    (Type $garnish_type:ident) => {
         {
             use garnish_lang_traits::GarnishDataType;
             BasicObject::Type(GarnishDataType::$garnish_type)
         }
     };
-    (Char($char_value:expr)) => {
+    (Char $char_value:expr) => {
         BasicObject::Char($char_value)
     };
-    (Byte($byte_value:expr)) => {
+    (Byte $byte_value:expr) => {
         BasicObject::Byte($byte_value)
     };
-    (Symbol($symbol_value:expr)) => {
+    (Symbol $symbol_value:expr) => {
         {
             use crate::basic::BasicDataFactory;
             use garnish_lang_traits::GarnishDataFactory;
@@ -41,16 +47,19 @@ macro_rules! basic_object {
     //         ),*
     //     ])
     // };
-    (External($value:expr)) => {
+    (Number $value:expr) => {
+        BasicObject::Number($value.into())
+    };
+    (External $value:expr) => {
         BasicObject::External($value)
     };
-    (Expression($value:expr)) => {
+    (Expression $value:expr) => {
         BasicObject::Expression($value)
     };
-    (CharList($value:expr)) => {
+    (CharList $value:expr) => {
         BasicObject::CharList($value.to_string())
     };
-    (ByteList($($value:expr),*)) => {
+    (ByteList $($value:expr),*) => {
         BasicObject::ByteList(vec![
             $($value),*
         ])
@@ -88,28 +97,35 @@ mod tests {
 
     #[test]
     fn garnish_type() {
-        let value: BasicObject = basic_object!(Type(Number));
+        let value: BasicObject = basic_object!(Type Number);
 
         assert_eq!(value, BasicObject::Type(GarnishDataType::Number))
     }
 
     #[test]
     fn build_char() {
-        let value: BasicObject = basic_object!(Char('a'));
+        let value: BasicObject = basic_object!(Char 'a');
 
         assert_eq!(value, BasicObject::Char('a'));
     }
 
     #[test]
+    fn build_number() {
+        let value: BasicObject = basic_object!(Number 100);
+
+        assert_eq!(value, BasicObject::Number(100.into()));
+    }
+
+    #[test]
     fn build_byte() {
-        let value: BasicObject = basic_object!(Byte(10));
+        let value: BasicObject = basic_object!(Byte 10);
 
         assert_eq!(value, BasicObject::Byte(10));
     }
 
     #[test]
     fn build_symbol() {
-        let value: BasicObject = basic_object!(Symbol("my_symbol"));
+        let value: BasicObject = basic_object!(Symbol "my_symbol");
 
         assert_eq!(value, BasicObject::Symbol(8904929874702161741));
     }
@@ -128,29 +144,61 @@ mod tests {
 
     #[test]
     fn build_external() {
-        let value: BasicObject = basic_object!(External(10));
+        let value: BasicObject = basic_object!(External 10);
 
         assert_eq!(value, BasicObject::External(10));
     }
 
     #[test]
     fn build_expression() {
-        let value: BasicObject = basic_object!(Expression(10));
+        let value: BasicObject = basic_object!(Expression 10);
 
         assert_eq!(value, BasicObject::Expression(10));
     }
 
     #[test]
     fn build_char_list() {
-        let value: BasicObject = basic_object!(CharList("value"));
+        let value: BasicObject = basic_object!(CharList "value");
 
         assert_eq!(value, BasicObject::CharList("value".to_string()));
     }
 
     #[test]
     fn build_byte_list() {
-        let value: BasicObject = basic_object!(ByteList(100, 200, 250));
+        let value: BasicObject = basic_object!(ByteList 100, 200, 250);
 
         assert_eq!(value, BasicObject::ByteList(vec![100, 200, 250]));
+    }
+
+    #[test]
+    fn build_pair() {
+        // Test simple values without parentheses
+        let value1: BasicObject = basic_object!(Unit = True);
+        assert_eq!(value1, BasicObject::Pair(Box::new(BasicObject::Unit), Box::new(BasicObject::True)));
+        
+        // Test Number = Number (requires parentheses for function-call syntax)
+        let value: BasicObject = basic_object!((Number 100) = (Number 200));
+        assert_eq!(value, BasicObject::Pair(Box::new(BasicObject::Number(100.into())), Box::new(BasicObject::Number(200.into()))));
+        
+        // Test different types on left and right
+        let value2: BasicObject = basic_object!((Char 'a') = (Number 42));
+        assert_eq!(value2, BasicObject::Pair(Box::new(BasicObject::Char('a')), Box::new(BasicObject::Number(42.into()))));
+        
+        // Test nested pairs (recursive) - each value in the inner pair needs parentheses
+        let value3: BasicObject = basic_object!(((Number 10) = (Number 20)) = (Number 30));
+        assert_eq!(value3, BasicObject::Pair(
+            Box::new(BasicObject::Pair(
+                Box::new(BasicObject::Number(10.into())),
+                Box::new(BasicObject::Number(20.into()))
+            )),
+            Box::new(BasicObject::Number(30.into()))
+        ));
+        
+        // Test pair with CharList
+        let value4: BasicObject = basic_object!((CharList "hello") = (Number 42));
+        assert_eq!(value4, BasicObject::Pair(
+            Box::new(BasicObject::CharList("hello".to_string())),
+            Box::new(BasicObject::Number(42.into()))
+        ));
     }
 }
